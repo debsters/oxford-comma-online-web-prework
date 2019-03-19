a06488d9b2988f943d6b61c816e57523f12afe01
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-19 16:09:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   else
-  array.insert(-2, 'and').join(", ")
+    before_and = array
+    before_and.slice!(-1)
+    before_and.join(", ").concat(", and ", array[-1])
   end
 end
