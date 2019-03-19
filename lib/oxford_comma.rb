52814152119e57ef264b24123536c7d294cf5a26
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-19 16:17:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,9 @@ def oxford_comma(array)
   elsif array.length == 2
     "".concat(array[0]," and ", array[1])
   else
+    last = array[-1]
     before_and = array
     before_and.slice!(-1)
-    before_and.join(", ").concat(", and ", array[-1])
+    before_and.join(", ").concat(", and ", last)
   end
 end
