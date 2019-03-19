1be4dbd3d24e379ee7d4d2e842e21be7892fc32c
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-19 16:15:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
+  elsif array.length == 2
+    "".concat(array[0]," and ", array[1])
   else
     before_and = array
     before_and.slice!(-1)
